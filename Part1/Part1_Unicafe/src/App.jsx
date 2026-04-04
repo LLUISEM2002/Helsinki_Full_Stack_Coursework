6a3d9dcf371e6299b8b54ff94e03745866f71801
Lluis Escolano <113066894+LLUISEM2002@users.noreply.github.com>
2026-04-03 19:38:51 -0700

    Finished 1.7

--- Part1/Part1_Unicafe/src/App.jsx
+++ Part1/Part1_Unicafe/src/App.jsx
@@ -7,8 +7,21 @@ const Button = (props) => (
     {props.text}
   </button>
 )
-const Print = (props) => <div>{props.text} {props.value}</div>
+const Print = (props) => <div>{props.text} {props.value} {props.symbol}</div>
 
+const Statistics = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+
+  return(
+    <div>
+      <Print text="All" value={good + neutral + bad} symbol="" />
+      <Print text="Average" value={(good - bad) / (good + neutral + bad)} symbol="" /> 
+      <Print text="Positive" value={good / (good + neutral + bad) * 100} symbol="%" />
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -25,6 +38,7 @@ const App = () => {
       <Print text="good" value={good} />
       <Print text="neutral" value={neutral} />
       <Print text="bad" value={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
 
   )
