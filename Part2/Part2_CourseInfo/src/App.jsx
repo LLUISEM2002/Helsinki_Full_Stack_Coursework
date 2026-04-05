cbb842a38d30ef89ba76e23499bd8bf260643452
Lluis Escolano <113066894+LLUISEM2002@users.noreply.github.com>
2026-04-04 18:36:51 -0700

    Finished 2.5 (Separated Course into a different component module along with the other components its dependent on)

--- Part2/Part2_CourseInfo/src/App.jsx
+++ Part2/Part2_CourseInfo/src/App.jsx
@@ -1,31 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = (props) => (
-  <div>
-    {props.parts.map(part => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-
-const Total = (props) => <p><b>total of {props.total} exercises</b></p>
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
