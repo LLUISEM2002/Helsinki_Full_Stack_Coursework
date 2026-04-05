3f9bcedd537f62bd6b5b3d33585cc419fea8e399
Lluis Escolano <113066894+LLUISEM2002@users.noreply.github.com>
2026-04-04 18:23:52 -0700

    fix forgot to implement so content allocates for any number of parts

--- Part2/Part2_CourseInfo/src/App.jsx
+++ Part2/Part2_CourseInfo/src/App.jsx
@@ -2,9 +2,9 @@ const Header = (props) => <h1>{props.course}</h1>
 
 const Content = (props) => (
   <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+    {props.parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
   </div>
 )
 
@@ -48,7 +48,7 @@ const App = () => {
     ]
   }
 
-  return <Course course={course} />
+  return <Course key={course.id} course={course} />
 }
 
 export default App
