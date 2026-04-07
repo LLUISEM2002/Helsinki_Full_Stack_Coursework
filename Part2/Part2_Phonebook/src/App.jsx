3bb7d04af69c4160cc44505b867452e64e9f097c
Lluis Escolano <113066894+LLUISEM2002@users.noreply.github.com>
2026-04-06 19:02:07 -0700

    Finished 2.10 Extracted all the logic components out of App. Also realized I was preventingDefault on changeHandlers when theyre only needed in form submission

--- Part2/Part2_Phonebook/src/App.jsx
+++ Part2/Part2_Phonebook/src/App.jsx
@@ -1,5 +1,45 @@
 import { useState } from 'react'
 
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown names: <input value={filter} onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
+  return (
+    <div>
+      <h2>Add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+
+const Persons = ({ persons, filter }) => {
+  return (
+    <ul>
+      {persons
+        .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+        .map(person => 
+          <li key={person.id}>{person.name}: {person.number}</li>
+        )}
+    </ul>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -12,13 +52,11 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
-    event.preventDefault()
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    event.preventDefault()
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
@@ -46,29 +84,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown names: <input value={filter} onChange={(event) => setFilter(event.target.value)} />
-      </div>
-      <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter filter={filter} handleFilterChange={(event) => setFilter(event.target.value)} />
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <ul>
-        {persons
-          .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-          .map(person => 
-            <li key={person.id}>{person.name}: {person.number}</li>
-          )}
-      </ul>
+      <Persons persons={persons} filter={filter} />
     </div>
     
   )
