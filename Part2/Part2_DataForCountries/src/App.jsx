a636e2851f96fcd8e2671e3b8918b3bc63f2f159
Lluis Escolano <113066894+LLUISEM2002@users.noreply.github.com>
2026-04-12 22:47:23 -0700

    Finished 2.18 Created the project and everything was going great until react had a spasm (for apparently no fault of my own) and would refuse to mount the app for which I had to literally debug for about 1.5h only to find out it fixed itself after closing the local host and rerunning npm run... I hate my life

---
+++ Part2/Part2_DataForCountries/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import countries from "./services/countries";
+
+const printList = (list) => {
+  return list.map((country, index) => <li key={country.name.common}>{country.name.common}</li>);
+}
+
+const App = () => {
+  const [countriesData, setCountriesData] = useState([])
+  const [search, setSearch] = useState('')
+
+  console.log("App rendering")
+
+  useEffect(() => {
+    console.log("useEffect fired")
+  }, [])
+
+  useEffect(() => {
+    countries.getAll().then(data => {
+    setCountriesData(data)
+  })
+}, [])
+
+const filteredCountries = countriesData.filter(country =>
+  country.name.common.toLowerCase().includes(search.toLowerCase())
+)
+
+const renderCountries = () => {
+  if (search === '') return null
+
+  if (filteredCountries.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+
+  if (filteredCountries.length > 1) {
+    return (
+      <ul>
+        {filteredCountries.map(country => (
+          <li key={country.name.common}>
+            {country.name.common}
+          </li>
+        ))}
+      </ul>
+    )
+  }
+
+  if (filteredCountries.length === 1) {
+    const country = filteredCountries[0]
+
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <p>Capital: {country.capital?.[0]}</p>
+        <p>Area: {country.area}</p>
+
+        <h2>Languages</h2>
+        <ul>
+          {Object.values(country.languages).map(lang => (
+            <li key={lang}>{lang}</li>
+          ))}
+        </ul>
+
+        <img src={country.flags.png} alt="flag" width="150" />
+      </div>
+    )
+  }
+  return <p>No matches found</p>
+}
+
+  return (
+    <div>
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search countries..."
+      />
+      {renderCountries()}
+    </div>
+  )
+}
+
+export default App;
